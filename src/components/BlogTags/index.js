9e7b0158eef76618afd75f3be44fe96ce1dd4836
Diogo Moreira <diogo.dmoreira@gmail.com>
2021-05-18 20:11:07 -0300

    Fixing spaces between tags

--- src/components/BlogTags/index.js
+++ src/components/BlogTags/index.js
@@ -4,16 +4,19 @@ import kebabCase from "lodash/kebabCase"
 // import AwesomeButton from "react-awesome-button"
 import { BlogTagsContainer, TagButton } from "./styled"
 import Content from "components/Content"
+import { StackItems } from "styles/globalStyles"
 
 const BlogTags = ({ tags }) => {
   return (
     <BlogTagsContainer>
       <Content>Navegue por tags:</Content>
-      {tags.map(tag => (
-        <Link to={`/tags/${kebabCase(tag.fieldValue)}`}>
-          <TagButton>{tag.fieldValue}</TagButton>
-        </Link>
-      ))}
+      <StackItems>
+        {tags.map(tag => (
+          <Link to={`/tags/${kebabCase(tag.fieldValue)}`}>
+            <TagButton>{tag.fieldValue}</TagButton>
+          </Link>
+        ))}
+      </StackItems>
     </BlogTagsContainer>
   )
 }
