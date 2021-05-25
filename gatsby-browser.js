ef472c394d4a318a5e4d616f3a25f227fd76bace
Diogo Moreira <diogo.dmoreira@gmail.com>
2021-05-25 18:10:51 -0300

    Adding transitions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,12 @@
-// import "./src/styles/main.scss"
-
-// import "jquery/dist/jquery.min.js"
-// import "popper.js/dist/popper.min"
+import React from "react"
+import { AnimatePresence } from "framer-motion"
 import "prismjs/themes/prism-twilight.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 import "prismjs/plugins/command-line/prism-command-line.css"
 
 import "katex/dist/katex.min.css"
-// import "bootstrap/dist/js/bootstrap.bundle.js"
 import "masonry-layout/dist/masonry.pkgd.js"
 
-// import "./static/fonts/neutrif.css"
+export const wrapPageElement = ({ element }) => (
+  <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
+)
