b4bb1f7f2382bd82545ef64e9ea0716ab6ac5bbe
Diogo Moreira <diogo.dmoreira@gmail.com>
2022-07-02 15:17:03 -0300

    Removing config/ folder, trying to make a better approach

--- config/social.config.ts
+++
@@ -1,8 +0,0 @@
-const social = [
-  { type: "twitter", link: "https://twitter.com/diogodmoreira" },
-  { type: "github", link: "https://github.com/diogomoreira" },
-  { type: "linkedin", link: "https://www.linkedin.com/in/diogodmoreira" },
-  { type: "instagram", link: "https://www.instagram.com/diogo.dmoreira" },
-];
-
-export default social;
