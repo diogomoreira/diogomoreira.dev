8d24f7643477b900f1d97f28d6bf4c4d79a67519
Diogo Moreira <diogo.dmoreira@gmail.com>
2022-07-02 15:17:03 -0300

    Removing config/ folder, trying to make a better approach

--- config/menu.config.ts
+++
@@ -1,8 +0,0 @@
-const menu = [
-  { name: "Blog", link: "/blog" },
-  { name: "Pesquisa", link: "/research" },
-  { name: "Labs", link: "/labs" },
-  { name: "Links", link: "/links" },
-];
-
-export default menu;
