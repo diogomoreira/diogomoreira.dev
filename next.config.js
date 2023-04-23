210eb9f0c6f30347267318075c9f5d1c58a2ce8f
Diogo Moreira <diogo.dmoreira@gmail.com>
2023-04-23 20:10:41 -0300

    Changing configuration files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const { i18n } = require("./next-i18next.config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
