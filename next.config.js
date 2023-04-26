b284918278a71cd00e61b8293f0d17c3541f322c
Diogo Moreira <diogo.dmoreira@gmail.com>
2023-04-26 14:51:07 -0300

    Fixing (?) Module not found error while loading the files from fs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  webpack: config => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
+// module.exports = nextConfig;
 module.exports = nextConfig;
