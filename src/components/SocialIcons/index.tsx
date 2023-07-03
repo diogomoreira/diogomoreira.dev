1ee27d069b912f4cb9861f3f7a74aadc8c5bf77f
Diogo Moreira <diogo.dmoreira@gmail.com>
2023-07-03 19:47:01 -0300

    Adding Mastodon

--- src/components/SocialIcons/index.tsx
+++ src/components/SocialIcons/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { useAppConfig } from "@/lib/config";
 import capitalize from "lodash/capitalize";
 import styles from "@/styles/components/socialicons.module.scss";
-import { faGithub, faInstagram, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faInstagram, faLinkedin, faMastodon, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { v4 as uuidv4 } from "uuid";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
 
 const SocialIcons = () => {
   const {
-    author: { instagram, github, twitter, linkedin, keybase },
+    author: { instagram, github, twitter, linkedin, keybase, mastodon },
   } = useAppConfig();
 
   const social = [
@@ -18,18 +18,13 @@ const SocialIcons = () => {
     { key: "twitter", link: `https://twitter.com/${twitter}`, icon: faTwitter },
     { key: "linkedin", link: `https://linkedin.com/in/${linkedin}`, icon: faLinkedin },
     { key: "keybase", link: `https://keybase.io/${keybase}`, icon: faKey },
+    { key: "mastodon", link: `https://mastodon.social/@${mastodon}`, icon: faMastodon },
   ];
 
   return (
     <div className={styles.socialIconsContainer}>
       {social.map(socialItem => (
-        <a
-          key={uuidv4()}
-          title={`Link for ${capitalize(socialItem.key)}`}
-          href={socialItem.link}
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a key={uuidv4()} title={`Link for ${capitalize(socialItem.key)}`} href={socialItem.link} rel="me">
           <FontAwesomeIcon icon={socialItem.icon} />
         </a>
       ))}
