290d966c17c5a1e4bfe69ae72b06448ab48ff271
Diogo Moreira <diogo.dmoreira@gmail.com>
2023-07-07 23:21:07 -0300

    Changing about page formatting

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,8 +3,10 @@ import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { NextSeo } from "next-seo";
 import styles from "@/styles/pages/about.module.scss";
+import contentStyle from "@/styles/pages/slug.module.scss";
 import Image from "next/image";
 import { useAppConfig } from "@/lib/config";
+import Link from "next/link";
 
 type AboutPageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -20,29 +22,34 @@ const AboutPage: NextPage<AboutPageProps> = () => {
       <div className={styles.cover}>
         <Image src={"/images/pages/about/cover.jpg"} fill alt={name} />
       </div>
-      <p>
-        For those who are new here, let me introduce myself. My name is <strong>Diogo Dantas Moreira</strong>, and I
-        hold a degree in <strong>System Analysis and Development</strong> from the{" "}
-        <a href="https://ifpb.edu.br">Federal Institute of Paraíba</a>, where I later worked as an adjunct professor. I
-        completed my specialization in <strong>Software Engineering</strong> in 2015 and my master&apos;s degree in the
-        same field from 2017 to 2019, during which I discovered my passion for <strong>Software Testing</strong>.
-        Currently, I teach subjects related to <strong>Software Engineering</strong>, <strong>Software Testing</strong>,
-        and <strong>Design Patterns</strong>.
-      </p>
-      <p>
-        Aside from work, I enjoy playing video games, particularly role-playing games. I also enjoy reading, although I
-        wish I had more time for it. I love watching anime, TV series, and movies, especially those related to science
-        fiction and fantasy themes. I&apos;m a football fan and support São Paulo FC. I also love traveling,
-        experiencing new cultures, trying new things, and most importantly, spending time with friends and family while
-        enjoying a good beer 🍻.
-      </p>
-      <p>
-        In terms of content, expect to see posts about web development and software testing, the areas I&apos;ve been
-        studying extensively lately. Gradually, I&apos;ll share more about my life and interests here.
-      </p>
-      <p>
-        I hope you enjoy the content I post, and I welcome any suggestions or requests in the comments section below. 😊
-      </p>
+      <div className={contentStyle.content}>
+        <p>
+          For those who are new here, let me introduce myself. My name is <strong>Diogo Dantas Moreira</strong>, born
+          and living in Brazil.
+        </p>
+        <p>
+          I enjoy playing video games, particularly role-playing games. I also enjoy reading, although I wish I had more
+          time for it. I love watching anime, TV series, and movies, especially those related to science fiction and
+          fantasy themes. Take a look at some of the things I&apos;m into lately <Link href={"/links"}>here</Link>.
+        </p>
+        <p>
+          I&apos;m a soccer fan and <b>São Paulo FC</b> supporter. Lately I&apos;ve been enjoying Formula 1 as well.
+        </p>
+        <p>
+          I also love traveling, experiencing new cultures, trying new things, and most importantly, spending time with
+          friends and family while enjoying a good beer 🍻.
+        </p>
+        <p>
+          I hold a degree in <strong>System Analysis and Development</strong> from the{" "}
+          <a href="https://ifpb.edu.br">Federal Institute of Paraíba</a>, where I later worked as an adjunct professor.
+          I completed my specialization in <strong>Software Engineering</strong> in 2015 and my master&apos;s degree in
+          the same field from 2017 to 2019, during which I discovered my passion for <strong>Software Testing</strong>.
+          Currently, I teach subjects related to <strong>Software Engineering</strong>,{" "}
+          <strong>Software Testing</strong>, and <strong>Design Patterns</strong>. You can expect to see posts about web
+          development and software testing here, the areas I&apos;ve been studying extensively lately.
+        </p>
+        <p>Gradually, I&apos;ll share more about my life and interests here.</p>
+      </div>
     </>
   );
 };
