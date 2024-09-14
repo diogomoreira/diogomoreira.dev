2efcdcefe75e4b0de6112c5094d882145199b506
Diogo Moreira <diogo.dmoreira@gmail.com>
2024-09-14 18:27:36 -0300

    Checking environment to resolve siteUrl

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,3 @@
-import { IconType } from "react-icons";
-import { FaBookmark, FaComputer, FaFlask, FaGraduationCap, FaHouseChimney, FaPenNib, FaUser } from "react-icons/fa6";
 import { locales } from "./locale.config";
 
 type ApplicationConfig = {
@@ -14,49 +12,46 @@ type ApplicationConfig = {
   menu: {
     name: string;
     link: string;
-    icon: IconType;
   }[];
+};
+
+const externalLinks: Record<string, string> = {
+  email: "mailto:diogo.dmoreira@gmail.com",
+  academicEmail: "mailto:diogo.moreira@ifpb.edu.br",
+  instagram: "https://instagram.com/diogo.dmoreira",
+  github: "https://github.com/diogomoreira",
+  x: "https://x.com/diogodmoreira",
+  linkedin: "https://linkedin.com/in/diogodmoreira",
+  mastodon: "https://mastodon.social/@diogomoreira",
+  googleScholar: "https://scholar.google.com.br/citations?hl=pt-BR&user=DlSdlvEAAAAJ",
+  researchGate: "https://www.researchgate.net/profile/Diogo_Moreira4",
+  orcid: "https://orcid.org/0000-0003-1803-6565",
+  devto: "https://dev.to/diogomoreira",
+  lattes: "https://lattes.cnpq.br/2745996619940977",
 };
 
 const appConfig: ApplicationConfig = {
   title: "Diogo Moreira",
   description: "My thoughts about life, software development, games, music and so forth",
   keywords: ["software development", "teaching"],
-  siteUrl: "https://diogomoreira.dev/",
+  siteUrl: process.env.NEXT_PUBLIC_CONTEXT === "production" ? "https://diogomoreira.dev/" : "http://localhost:3000",
   siteLocale: locales.en.code,
-  siteVersion: "3.2",
+  siteVersion: "3.3",
   repository: "https://github.com/diogomoreira/diogomoreira.dev",
   author: {
     name: "Diogo Moreira",
-    image: "/images/profile_new.jpg",
-    email: "diogo.dmoreira@gmail.com",
-    academicEmail: "diogo.moreira@ifpb.edu.br",
-    github: "diogomoreira",
-    twitter: "diogodmoreira",
-    linkedin: "diogodmoreira",
-    instagram: "diogo.dmoreira",
-    mastodon: "diogomoreira",
-    googleScholar: "DlSdlvEAAAAJ",
-    researchGate: "Diogo_Moreira4",
-    orcid: "0000-0003-1803-6565",
-    lattes: "2745996619940977",
+    image: "/images/profile/front.jpg",
+    ...externalLinks,
   },
   menu: [
-    { name: "menu.home", link: "/", icon: FaHouseChimney },
-    { name: "menu.about", link: "/about", icon: FaUser },
-    { name: "menu.blog", link: "/blog", icon: FaPenNib },
-    {
-      name: "menu.academic",
-      link: "/academic",
-      icon: FaGraduationCap,
-    },
-    {
-      name: "menu.uses",
-      link: "/uses",
-      icon: FaComputer,
-    },
-    { name: "menu.labs", link: "/labs", icon: FaFlask },
-    { name: "menu.bookmarks", link: "/bookmarks", icon: FaBookmark },
+    { name: "menu.home", link: "/" },
+    { name: "menu.about", link: "/about" },
+    { name: "menu.cv", link: "/cv" },
+    { name: "menu.blog", link: "/blog" },
+    { name: "menu.academic", link: "/academic" },
+    { name: "menu.uses", link: "/uses" },
+    { name: "menu.projects", link: "/projects" },
+    { name: "menu.bookmarks", link: "/bookmarks" },
   ],
 };
 
